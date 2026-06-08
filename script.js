4ccefd2013d0cbb72eaeb04737be1ce1397a12cc
Ayush Sharma <79994281+Ayushsharma2003@users.noreply.github.com>
2026-06-08 12:08:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("contact-form");
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", async function(event){
 
     event.preventDefault();
 
@@ -13,5 +13,27 @@ form.addEventListener("submit", function(event){
         return;
     }
 
-    alert("Form submitted successfully!");
+    try{
+
+        await fetch(
+            "https://script.google.com/macros/s/AKfycbzlpX221aCRBTYLFrUAcN_61HXMCX_DDXiC680W4Lx6t0DB4FKa-PQKR6dO0G1QTQuT9A/exec",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    message: message
+                })
+            }
+        );
+
+        alert("Message submitted successfully!");
+        form.reset();
+
+    }catch(error){
+
+        alert("Something went wrong!");
+
+    }
+
 });
